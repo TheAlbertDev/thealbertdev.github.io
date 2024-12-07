bc9e83aaa6b011ddd4cc597b47584554ca6951d9
Albert Álvarez-Carulla <hello@thealbert.dev>
2024-12-07 11:29:27 +0100

    wip

--- ec.config.mjs
+++ ec.config.mjs
@@ -3,5 +3,8 @@ import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-s
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 export default defineEcConfig({
+  themes: ['github-dark-dimmed', 'github-light'],
+  themeCssSelector: (theme) => `[data-code-theme='${theme.name}']`,
+  useDarkModeMediaQuery: false,
   plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
 });
